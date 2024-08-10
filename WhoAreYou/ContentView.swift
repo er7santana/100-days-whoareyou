a5c7c6de7ac1c7cf030b50e808524561c1664340
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-10 10:53:42 -0300

    Initial Commit

---
+++ WhoAreYou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhoAreYou
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 10/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhoAreYou/WhoAreYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAreYouApp.swift
+//  WhoAreYou
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WhoAreYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
